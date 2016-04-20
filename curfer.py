27d3d470f79b24590d1a6a655b39c1f29e2ee80a
Henning Hasemann <henning@hasemail.de>
2016-04-20 17:06:23 +0200

    cleanup curfer.py

--- curfer.py
+++ curfer.py
@@ -6,6 +6,7 @@ from tracking_pb2 import Track, TrackEntry
 
 def read_data(filename):
     """
+    filename: filename of a curfer -data file.
     Yields TrackEntry objects
     """
     file_ = open(filename, 'rb')
@@ -25,6 +26,10 @@ def read_data(filename):
 
 
 def generate_ttp(trace):
+    """
+    trace: iterable over curfer TrackEntry objects.
+    returns: TTP suitable for positioning & mapmatching as string.
+    """
 
     # For TTP spec see
     # https://confluence.tomtomgroup.com/display/POS/How+to+read+ttp+log+files
@@ -63,130 +68,5 @@ def generate_ttp(trace):
         r += l
     r += 'END\n'
     return r
-
-
-
-
-
-
-
-
-# ----
-
-def read_metafile(file_):
-    block = file_.read()
-
-    entry = Track()
-    entry.ParseFromString(block)
-
-    return entry
-
-
-def generate_gmaps(traces):
-
-    trips = []
-
-    for trace in traces:
-        l = []
-        for e in trace:
-            try:
-                l.append((e.latitude, e.longitude))
-            except KeyError:
-                pass
-        trips.append(l)
-
-    r = '''
-<!DOCTYPE html>
-<html>
-<head>
-<script
-src="http://maps.googleapis.com/maps/api/js">
-</script>
-
-<script>
-
-var trips = [
-    {}
-    ];
-
-var colors = [
-    "#000000",
-    "#ff0000", "#00ff00", "#0000ff",
-    "#ff00ff", "#ffff00", "#ffffff"
-    ];
-
-function initialize()
-{{
-    var mapProp = {{
-        center: trips[0][0],
-        zoom:15,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    }};
-      
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-    for(var i = 0; i < trips.length; i++) {{
-        var flightPath=new google.maps.Polyline({{
-          path: trips[i],
-          strokeColor: colors[i % colors.length],
-          strokeOpacity:0.8,
-          strokeWeight:2
-          }});
-
-        flightPath.setMap(map);
-    }}
-
-
-}}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-</head>
-
-<body>
-<div id="googleMap" style="width:1000px;height:800px;"></div>
-</body>
-</html>
-'''.format(','.join('[' + 
-            ','.join(('new google.maps.LatLng({}, {})'.format(lat, lon) for (lat, lon) in trip))
-            + ']' for trip in trips))
-
-    return r
-
-if __name__ == '__main__':
-
-    # datafile
-
-    traces = []
-
-    for filename in sys.argv[1:]:
-        traces.append(list(read_data(filename)))
-
-    maps_outname = 'out_gmaps.html'
-    print('writing ' + maps_outname)
-    with open(maps_outname, 'w') as mapsfile:
-        mapsfile.write(generate_gmaps(traces))
-
-
-    ttp_outname = 'out.ttp'
-    print('writing ' + ttp_outname)
-    with open(ttp_outname, 'w') as ttpfile:
-        ttpfile.write(generate_ttp(traces))
-
-
-
-    #print('writing ' + data_outname)
-    #with open(data_outname, 'w') as dataoutfile:
-        #for entry in entries:
-            #dataoutfile.write(str(entry))
-            #dataoutfile.write('----------------\n')
-
-    # metafile
-
-    #print('writing ' + meta_outname)
-    #with open(metaname, 'rb') as metafile:
-        #with open(meta_outname, 'w') as metaoutfile:
-            #track = read_metafile(metafile)
-            #metaoutfile.write(str(track))
 
 
