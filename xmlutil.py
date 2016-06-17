8b5c227900c6eb80b59a1e5b7818a9eed3d18e23
Henning Hasemann <henning@hasemail.de>
2016-06-17 14:29:02 +0200

    Handle the case wer ele.attrib[x] is None

--- xmlutil.py
+++ xmlutil.py
@@ -1,12 +1,12 @@
 
 def element_text(ele, name, ctor = lambda x: x, default = None, ns = {}):
     e = ele.find(name, ns)
-    if e is None:
+    if e is None or e.text is None:
         return default
     return ctor(e.text)
 
 def attr_text(ele, name, ctor = lambda x: x, default = None, ns = {}):
-    if name not in ele.attrib:
+    if name not in ele.attrib or ele.attrib[name] is None:
         return default
     return ctor(ele.attrib[name])
 
