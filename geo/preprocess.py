bee0ff8a29e731a5b6b9d3205e8b9797915d0bbf
Henning Hasemann <henning@hasemail.de>
2017-12-06 11:12:41 +0100

    polynomial-based regression smoothing

--- geo/preprocess.py
+++ geo/preprocess.py
@@ -1,14 +1,55 @@
 
+import metrics
 
-def smooth(trace, dt):
+def add_t_uniformly(path, dt = 1.0):
+    # TODO: copy path, add a 't' column with 0,dt,2dt,...
+    pass
+
+def add_t_from_poly_distance(path, metric = metrics.geo):
+    # TODO: copy path, add a 't' column according to distance along polygon
+    pass
+
+
+def smooth_regress(path, dt, order):
+    """
+    path: data frame with at least columns 't', 'lat', 'lon'
+    dt: resampling time interval
+    order: order of the polynomial to use
+    return: interpolated path
+    """
+    import pandas as pd
+    import numpy as np
+    from numpy import polyfit, poly1d
+
+    start = path.t.iloc[0]
+    end = path.t.iloc[-1]
+    # new ts sequence
+    nt = start + np.linspace(0, end - start, (end - start) / dt + 1)
+
+    lat = np.poly1d(np.polyfit(path['t'], path['lat'], order))
+    lon = np.poly1d(np.polyfit(path['t'], path['lon'], order))
+
+    r = pd.DataFrame(columns = ('t', 'lat', 'lon'))
+    r['t'] = nt
+    r['lat'] = list(map(lat, nt))
+    r['lon'] = list(map(lon, nt))
+    r.set_index('t', inplace=True)
+
+    print("r=", r)
+    return r
+
+
+def smooth_spline(path, dt):
     """
     Simple smoothing & resampling of trace using splines.
     Not well-suited for noise or duplicates.
 
-    trace: data frame with at least columns 't', 'lat', 'lon'
+    path: data frame with at least columns 't', 'lat', 'lon'
     dt: resampling time interval
-    return: ???
+    return: interpolated path
 
+    TODO: allow to work without 't' attribute (and use distance instead),
+          as that is the usecase for map arcs
 
     >>> import pandas as pd
     >>> df = pd.DataFrame(data = { 't': [1, 2, 3, 4, 5], 'lat': [10, 12, 10, 9, 8], 'lon': [100, 99, 98, 95, 97] })
@@ -19,15 +60,15 @@ def smooth(trace, dt):
     import pandas as pd
     import numpy as np
 
-    start = trace.t.iloc[0]
-    end = trace.t.iloc[-1]
+    start = path.t.iloc[0]
+    end = path.t.iloc[-1]
+    # new ts sequence
     nt = start + np.linspace(0, end - start, (end - start) / dt + 1)
-
 
     r = pd.DataFrame(columns = ('t', 'lat', 'lon'))
     r['t'] = nt
-    r['lat'] = scipy.interpolate.spline(trace.t, trace.lat, nt)
-    r['lon'] = scipy.interpolate.spline(trace.t, trace.lon, nt)
+    r['lat'] = scipy.interpolate.spline(path.t, path.lat, nt)
+    r['lon'] = scipy.interpolate.spline(path.t, path.lon, nt)
     r.set_index('t', inplace=True)
 
     return r
--- geo/test.py
+++ geo/test.py
@@ -1,6 +1,6 @@
 
-from plot import plot_trace
-from preprocess import smooth
+from plot import plot_trace_points, plot_trace_path
+from preprocess import smooth_spline, smooth_regress
 
 import pandas as pd
 
@@ -8,13 +8,16 @@ from ggplot import ggplot, aes, geom_point, geom_line
 
 if __name__ == '__main__':
     df = pd.DataFrame(data = { 't': [1, 2, 3, 4, 5], 'lat': [10, 12, 10, 9, 8], 'lon': [100, 99, 98, 95, 97] })
-    df2 = smooth(df, 0.1)
+    df2 = smooth_spline(df, 0.1)
+    df3 = smooth_regress(df, 0.1, 4)
+    df4 = smooth_regress(df, 0.1, 3)
 
     p = ggplot(aes(x='lat', y='lon'), data=pd.DataFrame(columns=('lat', 'lon'), data={}))
 
-    p += plot_trace(df, color='red')
-    print(df2)
-    p += plot_trace(df2)
+    p += plot_trace_points(df, color='black')
+    p += plot_trace_path(df2, color='red')
+    p += plot_trace_path(df3, color='green')
+    p += plot_trace_path(df4, color='blue')
 
     p.save('test.png')
 
