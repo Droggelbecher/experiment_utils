361393164ba13a1d93e9f25c8a3005f15cdf1ece
Henning Hasemann <henning@hasemail.de>
2018-04-23 19:31:30 +0200

    format_table: dont show horiz line w/o headers

--- experiment_utils/text.py
+++ experiment_utils/text.py
@@ -2,13 +2,22 @@
 import numpy as np
 
 def format_table(t, headers = None):
+    """
+    >>> print(format_table([ ['a', 'beeeee', 'cee'], [5678, 78, 123] ]))
+    a    beeeee cee
+    5678 78     123
+    <BLANKLINE>
+    """
+    t = tuple(t)
 
     if headers is None:
-        if not len(t):
-            return ''
-        headers = [''] * len(t[0])
+        max_widths = np.array([0 for x in t[0]])
+    else:
+        #if not len(t):
+        #    return ''
+        #headers = [''] * len(t[0])
+        max_widths = np.array([len(x) for x in headers])
 
-    max_widths = np.array([len(x) for x in headers])
     for row in t:
         max_widths = np.maximum(max_widths, [len(str(x)) for x in row])
 
@@ -21,8 +30,10 @@ def format_table(t, headers = None):
             for r, m in zip(row, max_widths)
         ) + '\n'
 
-    produce_row(headers)
-    produce_row([''] * len(headers), fill = '-')
+    if headers is not None:
+        produce_row(headers)
+        produce_row([''] * len(headers), fill = '-')
+
     for row in t:
         produce_row(row)
 
